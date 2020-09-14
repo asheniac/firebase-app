7c333fd9b3296b4c8f5005d101b5d8c7bd3449bf
Asheniac <asheniac@gmail.com>
2020-09-14 22:01:34 +1000

    login form

--- src/Components/LoginForm.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class LoginForm extends Component {
-  render() {
-    return <div>This is LoginForm</div>;
-  }
-}
-
-export default LoginForm;
--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Switch, BrowserRouter, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import LoginFrom from "./Components/LoginForm";
+import Login from "./Components/Login";
 import Contact from "./Components/Contact";
 import HomePage from "./Components/HomePage";
 
@@ -14,7 +14,7 @@ class Routes extends Component {
           <Header />
           <Switch>
             <Route exact path="/" component={HomePage} />
-            <Route exact path="/login" component={LoginFrom} />
+            <Route exact path="/login" component={Login} />
             <Route exact path="/contact" component={Contact} />
           </Switch>
           <Footer />
