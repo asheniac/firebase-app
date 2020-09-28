ca7515c665bf138c55e1790bac7c4d080431b5d2
Ash Chen <asheniac@gmail.com>
2020-09-28 21:25:22 +1000

    Update fBase.js

--- src/utils/fBase.js
+++ src/utils/fBase.js
@@ -3,14 +3,7 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyAhcoDQTzg35SaGs1JO2UXH75se3bCYCag",
-  authDomain: "fir-testing-22cae.firebaseapp.com",
-  databaseURL: "https://fir-testing-22cae.firebaseio.com",
-  projectId: "fir-testing-22cae",
-  storageBucket: "fir-testing-22cae.appspot.com",
-  messagingSenderId: "881093215320",
-  appId: "1:881093215320:web:0dc506eb755643a34bb62d",
-  measurementId: "G-RTNEXGWDG6",
+  
 };
 
 const firebaseApp = firebase.initializeApp(config);
